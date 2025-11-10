683fbf2f4589921c972a89e3cc616c7372a1fb4c
Monday David Osakpawan <rynstheoverlord@gmail.com>
2025-11-10 16:09:52 +0100

    Added initial support for commands

--- ballad.h
+++ ballad.h
@@ -2,6 +2,7 @@
 #define BALLAD_H
 
 typedef struct {
+  char *name;
   char *help;
 } BalladCommand;
 
@@ -16,7 +17,7 @@ typedef struct {
 
 BalladCli ballad_init(int charc, char **charv, char *help);
 void ballad_run(BalladCli cli);
-
+void ballad_add_command(BalladCli *cli, BalladCommand command);
 #endif
 
 #ifdef BALLAD_IMPLEMENTATION
@@ -31,8 +32,25 @@ BalladCli ballad_init(int charc, char **charv, char *help) {
 
 void ballad_run(BalladCli cli) {
   printf("Usage: %s COMMAND [OPTIONS] [ARGS]"
-         "\n\n\t%s\n\nOptions:\n\t--help\tShow this message and exit",
+         "\n\n\t%s\n\nOptions:\n\t--help, -h  Show this message and exit.\n",
          cli.charv[0], cli.help);
+
+  printf("\nCommands:\n");
+  for (int i = 0; i < cli.index; i++) {
+    BalladCommand command = cli.buffer[i];
+    printf("\t%s\t%s\n", command.name, command.help);
+  }
+  free(cli.buffer);
+}
+
+void ballad_add_command(BalladCli *cli, BalladCommand command) {
+  if (cli->index > cli->capacity - 1) {
+    cli->capacity *= 2;
+    cli->buffer = (BalladCommand *)realloc(
+        cli->buffer, cli->capacity * sizeof(BalladCommand));
+  }
+  cli->buffer[cli->index] = command;
+  cli->index += 1;
 }
 
 #endif
