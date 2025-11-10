4c178a50045e97662954a359519d596f7687a431
Monday David Osakpawan <rynstheoverlord@gmail.com>
2025-11-10 16:44:13 +0100

    Added error handling

--- ballad.h
+++ ballad.h
@@ -4,6 +4,7 @@
 typedef struct {
   char *name;
   char *help;
+  void *action;
 } BalladCommand;
 
 typedef struct {
@@ -18,9 +19,13 @@ typedef struct {
 BalladCli ballad_init(int charc, char **charv, char *help);
 void ballad_run(BalladCli cli);
 void ballad_add_command(BalladCli *cli, BalladCommand command);
+void ballad_print(BalladCli cli);
+char *ballad_get_arg(int charc, char **charv, int index);
+void ballad_invalid_command_print(char *name, BalladCli cli);
 #endif
 
 #ifdef BALLAD_IMPLEMENTATION
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -31,14 +36,21 @@ BalladCli ballad_init(int charc, char **charv, char *help) {
 }
 
 void ballad_run(BalladCli cli) {
-  printf("Usage: %s COMMAND [OPTIONS] [ARGS]"
-         "\n\n\t%s\n\nOptions:\n\t--help, -h  Show this message and exit.\n",
-         cli.charv[0], cli.help);
+  if (cli.charc < 2) {
+    ballad_print(cli);
+  } else {
+    bool arg_exists;
+    for (int i = 0; i < cli.index; i++) {
+      if (ballad_get_arg(cli.charc, cli.charv, 1) == cli.buffer[i].name) {
+        arg_exists = true;
+      }
+      printf("%s\n", cli.buffer[i].name);
+    }
 
-  printf("\nCommands:\n");
-  for (int i = 0; i < cli.index; i++) {
-    BalladCommand command = cli.buffer[i];
-    printf("\t%s\t%s\n", command.name, command.help);
+    if (!arg_exists) {
+      ballad_invalid_command_print(ballad_get_arg(cli.charc, cli.charv, 1),
+                                   cli);
+    }
   }
   free(cli.buffer);
 }
@@ -53,4 +65,31 @@ void ballad_add_command(BalladCli *cli, BalladCommand command) {
   cli->index += 1;
 }
 
+void ballad_print(BalladCli cli) {
+  printf("Usage: %s COMMAND [OPTIONS] [ARGS]"
+         "\n\n\t%s\n\nOptions:\n\t--help, -h  Show this message and exit.\n",
+         cli.charv[0], cli.help);
+
+  printf("\nCommands:\n");
+  for (int i = 0; i < cli.index; i++) {
+    BalladCommand command = cli.buffer[i];
+    printf("\t%s\t%s\n", command.name, command.help);
+  }
+}
+
+char *ballad_get_arg(int charc, char **charv, int index) {
+  // dunno why this function exists tbh i was trying to do the golang thing
+  // didn't quite work out lmao
+
+  if (charc > index)
+    return charv[index];
+  else
+    return "";
+}
+
+void ballad_invalid_command_print(char *name, BalladCli cli) {
+  printf("Usage: %s COMMAND [OPTIONS] [ARGS]\n", cli.charv[0]);
+  printf("Try '%s --help' for help\n\n", cli.charv[0]);
+  printf("No such command '%s'\n", name);
+}
 #endif
