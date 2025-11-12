f14a4f9ef4f57c8fcda741e8f555224a532d11f1
Monday David Osakpawan <rynstheoverlord@gmail.com>
2025-11-12 09:16:36 +0100

    Added initial flag support and reformated code

--- ballad.h
+++ ballad.h
@@ -91,10 +91,31 @@
 
 // ballad declarations 
 
+#include <stdbool.h> 
+
+typedef enum {BOOLEAN_FLAG, STRING_FLAG, INTEGER_FLAG} FlagType;
+
+typedef struct {
+	char short_name;
+	char *long_name;
+	FlagType f_type;
+	union {
+		bool b_flag;
+		char *s_flag;
+		int *i_flag;
+	};
+} BalladFlag;
+
+typedef struct {
+	BalladFlag *flags;
+	int len;
+} BalladFlagBox;
+
 typedef struct {
   char *name;
   char *help;
   void (*action)(char *arg);
+  BalladFlagBox flag_box;
 } BalladCommand;
 
 typedef struct {
@@ -122,77 +143,105 @@ void ballad_log_danger(char *message);
 #endif
 
 #ifdef BALLAD_IMPLEMENTATION
-#include "color_codes.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-BalladCli ballad_init(int charc, char **charv, char *help) {
-  return (BalladCli){help,  (BalladCommand *)malloc(5 * sizeof(BalladCommand)),
-                     5,     0,
-                     charc, charv};
+BalladCli ballad_init(int charc, char **charv, char *help) 
+{
+    return (BalladCli){help,  (BalladCommand *)malloc(5 * sizeof(BalladCommand)), 5, 0, charc, charv};
 }
 
-void ballad_run(BalladCli cli) {
-  if (cli.charc < 2) {
-    ballad_print(cli);
-  } else {
-    bool arg_exists = false;
-    for (int i = 0; i < cli.index; i++) {
-      if (strcmp(ballad_get_arg(cli.charc, cli.charv, 1), cli.buffer[i].name) ==
-          0) {
-        arg_exists = true;
-        if (cli.buffer[i].action != NULL) {
-          cli.buffer[i].action(ballad_get_arg(cli.charc, cli.charv, 2));
+void ballad_run(BalladCli cli) 
+{
+    if (cli.charc < 2) 
+    {
+        ballad_print(cli);
+    } else 
+    {
+        bool arg_exists = false;
+        for (int i = 0; i < cli.index; i++) 
+        {
+            if (strcmp(ballad_get_arg(cli.charc, cli.charv, 1), cli.buffer[i].name) ==0) 
+            {
+                arg_exists = true;
+
+                BalladFlagBox flag_box = cli.buffer[i].flag_box;
+                for (int i=0; i < flag_box.len; i++ ) 
+                {
+                    BalladFlag flag = flag_box.flags[i];
+                    for (int i=0; i < cli.charc; i++) 
+                    {
+                        if (i > 2) 
+                        {
+                            char conv[3] = "";
+                            sprintf(conv, "-%c", flag.short_name);
+                            if (strcmp(cli.charv[i], conv) == 0) 
+                            {
+                                printf("Flag found!\n");
+                            } else {
+                                printf("Incorrect usage: flag provided but not defined: %s\n", cli.charv[i]);
+                                break;
+                            }
+                        }
+                    }	
+                }
+
+                if (cli.buffer[i].action != NULL) {
+                    cli.buffer[i].action(ballad_get_arg(cli.charc, cli.charv, 2));
+                }
+            }
         }
-      }
-    }
 
-    if (!arg_exists) {
-      ballad_invalid_command_print(ballad_get_arg(cli.charc, cli.charv, 1),
+        if (!arg_exists) {
+            ballad_invalid_command_print(ballad_get_arg(cli.charc, cli.charv, 1),
                                    cli);
+        }
     }
-  }
-  free(cli.buffer);
+    free(cli.buffer);
 }
 
-void ballad_add_command(BalladCli *cli, BalladCommand command) {
-  if (cli->index > cli->capacity - 1) {
-    cli->capacity *= 2;
-    cli->buffer = (BalladCommand *)realloc(
-        cli->buffer, cli->capacity * sizeof(BalladCommand));
-  }
-  cli->buffer[cli->index] = command;
-  cli->index += 1;
+void ballad_add_command(BalladCli *cli, BalladCommand command) 
+{
+    if (cli->index > cli->capacity - 1) 
+    {
+        cli->capacity *= 2;
+        cli->buffer = (BalladCommand *)realloc(cli->buffer, cli->capacity * sizeof(BalladCommand));
+    }
+    cli->buffer[cli->index] = command;
+    cli->index += 1;
 }
 
-void ballad_print(BalladCli cli) {
-  printf("Usage: %s COMMAND [OPTIONS] [ARGS]"
+void ballad_print(BalladCli cli) 
+{
+    printf("Usage: %s COMMAND [OPTIONS] [ARGS]"
          "\n\n\t%s\n\nOptions:\n\t--help, -h  Show this message and exit.\n",
          cli.charv[0], cli.help);
-
-  printf("\nCommands:\n");
-  for (int i = 0; i < cli.index; i++) {
-    BalladCommand command = cli.buffer[i];
-    printf("\t%s\t%s\n", command.name, command.help);
-  }
+    printf("\nCommands:\n");
+    for (int i = 0; i < cli.index; i++) 
+    {
+        BalladCommand command = cli.buffer[i];
+        printf("\t%s\t%s\n", command.name, command.help);
+    }
 }
 
-char *ballad_get_arg(int charc, char **charv, int index) {
-  // dunno why this function exists tbh i was trying to do the golang thing
-  // didn't quite work out lmao
-
-  if (charc > index)
-    return charv[index];
-  else
-    return "";
+char *ballad_get_arg(int charc, char **charv, int index) 
+{
+    // dunno why this function exists tbh i was trying to do the golang thingy
+    // didn't quite work out lmao
+    
+    if (charc > index)
+        return charv[index];
+    else
+        return "";
 }
 
-void ballad_invalid_command_print(char *name, BalladCli cli) {
-  printf("Usage: %s COMMAND [OPTIONS] [ARGS]\n", cli.charv[0]);
-  printf("Try '%s --help' for help\n\n", cli.charv[0]);
-  printf("No such command '%s'\n", name);
+void ballad_invalid_command_print(char *name, BalladCli cli) 
+{
+    printf("Usage: %s COMMAND [OPTIONS] [ARGS]\n", cli.charv[0]);
+    printf("Try '%s --help' for help\n\n", cli.charv[0]);
+    printf("No such command '%s'\n", name);
 }
 
 // logging functions definitions
